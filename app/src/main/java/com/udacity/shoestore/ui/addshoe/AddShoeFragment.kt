13f4027ef45eb7248795b97d913e10c17944abf0
Matt Cushing <matt.cushing@eclypses.com>
2020-12-13 23:05:14 -0700

    Fixed issue where I needed to add two-way databinding to all EditText views according to code review

--- app/src/main/java/com/udacity/shoestore/ui/addshoe/AddShoeFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ui/addshoe/AddShoeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.data.models.Shoe
@@ -22,33 +23,15 @@ class AddShoeFragment : Fragment() {
         binding = FragmentAddShoeBinding.inflate(layoutInflater, container, false)
         viewModel = ViewModelProvider(requireActivity()).get(MainActivityViewModel::class.java)
 
-        binding.addshoeSaveButton.setOnClickListener { _: View ->
-            val name = when(binding.addshoeInputName.text.toString() == "") {
-                true -> "N/A"
-                false -> binding.addshoeInputName.text.toString()
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+
+        viewModel.isShoeAdded.observe(viewLifecycleOwner, Observer {
+            if (it) {
+                viewModel.shoeAddedComplete()
+                findNavController().popBackStack()
             }
-
-            val company = when(binding.addshoeInputCompany.text.toString() == "") {
-                true -> "N/A"
-                false -> binding.addshoeInputCompany.text.toString()
-            }
-
-            val size = when(binding.addshoeInputSize.text.toString() == "") {
-                true -> 0.0
-                false -> binding.addshoeInputSize.text.toString().toDouble()
-            }
-
-            val description = when(binding.addshoeInputDescription.text.toString() == "") {
-                true -> "N/A"
-                false -> binding.addshoeInputDescription.text.toString()
-            }
-
-            val newShoe = Shoe(name, size, company, description)
-
-            viewModel.addShoe(newShoe)
-
-            findNavController().popBackStack()
-        }
+        })
 
         binding.addshoeCancelButton.setOnClickListener { findNavController().popBackStack() }
 
